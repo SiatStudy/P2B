6aac4753bc561387c632bb9e3a19fa5d8d89a1bd
Kevin Rhie <Linea89kr@gmail.com>
2023-08-03 15:16:56 +0900

    [황수] P1B 폴더 딸려온 거 제거.

--- src/main/java/com/example/P1B/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.P1B.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
